496514ddd07cdde1329ae9e629ca0cb848e4ee51
Dustin Ancalade <umbi24@gmail.com>
2020-03-31 21:31:05 -0700

    all four services render from proxy

--- client/src/proxy.jsx
+++ client/src/proxy.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import css from './styles.css'
 
 
-const Proxy = () => {
-  return (<div>Proxy Service is Rendering</div>)
-};
+class Proxy extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {productNumber: '5'};
+  }
 
+  // componentDidMount() {
+  //   this.getProduct();
+
+  // }
+  // getProduct() {
+  //   $.ajax('/products', {
+  //     success: (prod) => {
+  //       console.log('prod', prod);
+  //       this.setState({productNumber: prod});
+  //     }
+  //   })
+
+
+  render() {
+
+
+    return (<div>Proxy Service is Rendering</div>);
+
+  }
+};
 
 
 ReactDom.render(<Proxy />, document.getElementById('proxy'));
