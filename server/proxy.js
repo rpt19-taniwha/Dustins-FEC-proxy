3c2bbc6ad5d86321f28e217bd77f3f6ac363d382
Dustin Ancalade <umbi24@gmail.com>
2020-04-11 12:56:54 -0700

    update

--- server/proxy.js
+++ server/proxy.js
@@ -3,16 +3,17 @@ const path = require('path');
 const cors = require('cors');
 
 const proxy = express();
+
+proxy.use(cors());
 const port = process.env.Port || 5000;
 
 proxy.use(express.json())
 proxy.use(express.urlencoded({extended: false}));
 proxy.use(express.static(path.join(__dirname, '/../public/')));
 
-proxy.use(cors());
 
 proxy.get('/product/:productNumber', (req, res) => {
-  console.log('req.url', req.url);
+  console.log('req.url', req.headers);
   res.sendFile('index.html', {
     root: path.join(__dirname, '/../public/'),
   });
