7770b19fb880244efc61b1dccdd54eb4b9549808
Dustin Ancalade <umbi24@gmail.com>
2020-04-24 18:42:57 -0700

    reset port numbers from testing to service

--- client/src/proxy.jsx
+++ client/src/proxy.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import $ from 'jquery';
 import css from './styles.css'
 
 
 class Proxy extends Component {
   constructor(props) {
     super(props);
-    this.state = {productNumber: '5'};
+    this.state = {productNumber: 549504785};
   }
+
+  // componentDidMount() {
+  //   $.ajax(`http://ec2-50-18-28-6.us-west-1.compute.amazonaws.com:8000/listing/549504785${this.state.productNumber}`, () => {
+  //     success: (data) => {
+  //       console.log('data', data);
+  //     }
+  //   });
+
+  // }
 
 
   render() {
